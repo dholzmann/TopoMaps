7d76417cc5faeb0fcec1e2e77062d368f2d6022f
Lukas Schubert <lukas.schubert@bertelsmann.de>
2022-11-17 09:19:44 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Hello PyCharm')
+
+    # 1 learn prototypes in w^c, clustering the input space using (SOM, ITM or GNG)
+    # 2 learn corresponding output vectors w_c^out
+    # 3 adapt corresponding linear map A_c
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
