ad7c3a260dd01c395d18c07759c314d8325e45fe
Dennis Holzmann <holzmann_dennis@web.de>
2022-11-19 14:05:13 +0100

    add network class

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
 
-class Node:
-
-
-class Network:
-    def __init__(self):
-        self.nodes = [] # list of nodes
-        self.edges = [] # list of edges
-        self.learning_rate
-        
-    def get_edges(node):
-    
-    def get_neighbouring_nodes(neighbor_func):
-        
-    def node_update():
-        # return delta w^in_c
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
